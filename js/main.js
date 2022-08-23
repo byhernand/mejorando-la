baaa0f053dc1ba3a2390809df0358adbaba0feb9
Eduardo Hernand <byhernand@hotmail.com>
2022-08-23 11:34:30 -0600

    Fetch data with Fetch

--- js/main.js
+++ js/main.js
@@ -3,46 +3,30 @@ const user = document.getElementById('user');
 const API = 'https://api.escuelajs.co/api/v1';
 
 
-function fetchData(urlApi, callback) {
-  const xhttp = new XMLHttpRequest();
-
-  xhttp.open('GET', urlApi, true);
-  xhttp.onreadystatechange = function(event) {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200) {
-        callback(null, JSON.parse(xhttp.responseText));
-      } else {
-        const err = new Error('Error' + urlApi);
-        console.log(err);
-      }
-    }
-  }
-
-  xhttp.send();
-}
-
-
-fetchData(`${API}/products`, function (error1, data1) {
-  if (error1) return console.log(error1);
-
-  fetchData(`${API}/users`, function (error2, data2) {
-    if (error2) return console.log(error2);
-
-    const productList = data1.slice(5, 25); // Getting 20 products
-    const cards = productList.map(item => `
+fetch(`${API}/products`)
+  .then(response => response.json())
+  .then(productList => {
+    const list = productList.slice(5, 25); // Getting 20 products
+    const cards = list.map(item => `
       <div>
         <img src="${item.images[0]}" alt="${item.description}" />
-        <h3>${item.title}</h3>
+        <p><b>${item.title}</b></p>
         <p>$ ${item.price}</p>
       </div>
     `);
-    const userInfo = `
-      <img src="${data2[0].avatar}" />
-      <p>User: ${data2[0].name}</p>
-      <p>Email: ${data2[0].email}</p>
-    `;
 
     products.innerHTML = cards.join('');
+
+    return fetch(`${API}/users`); // Getting user info
+  })
+  .then(response => response.json())
+  .then(userList => {
+    const userInfo = `
+      <img src="${userList[0].avatar}" />
+      <p><b>User: ${userList[0].name}</b></p>
+      <p>Email: ${userList[0].email}</p>
+    `;
+
     user.innerHTML = userInfo;
   })
-})
+  .catch(err => console.log(err));
